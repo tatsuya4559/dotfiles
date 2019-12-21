50ce076eab38e1370ed767e32dec3635f5ee0b6f
tatsuya4559 <tatsuya.k1029@gmail.com>
2019-12-21 16:27:20 +0900

    mac設定はいらなくなった

--- setup_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
