6f5ee29f63d27cc64f56a3a8413b41ad5c36ca1e
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-09-10 17:57:39 +0900

    refactor

--- setup_mac.sh
+++ setup_mac.sh
@@ -3,13 +3,13 @@
 # clone dotfiles
 echo 'Cloning dotfiles...'
 if [[ ! -d $HOME/dotfiles ]]; then
-  git clone https://github.com/tatsuya4559/dotfiles.git $HOME/dotfiles
+  git clone https://github.com/tatsuya4559/dotfiles.git "$HOME/dotfiles"
 fi
-cd $HOME/dotfiles
+cd "$HOME/dotfiles" || exit 1
 
 # install homebrew
 echo 'Installing homebrew...'
-if [[ -z `command -v brew` ]]; then
+if [[ -z $(command -v brew) ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 brew update
@@ -22,13 +22,13 @@ brew bundle
 # install git utils
 if [[ ! -f $HOME/.git-completion.bash ]]; then
   echo 'download .git-completion.bash'
-  curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash
-  chmod a+x $HOME/.git-completion.bash
+  curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$HOME/.git-completion.bash"
+  chmod a+x "$HOME/.git-completion.bash"
 fi
 if [[ ! -f $HOME/.git-prompt.sh ]]; then
   echo 'download .git-prompt.sh'
-  curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o $HOME/.git-prompt.sh
-  chmod a+x $HOME/.git-prompt.sh
+  curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$HOME/.git-prompt.sh"
+  chmod a+x "$HOME/.git-prompt.sh"
 fi
 
 # distribute dotfiles
@@ -38,7 +38,7 @@ copy() {
     echo "skip copying: $HOME/$1 exists"
   else
     echo "copy $HOME/dotfiles/$1 to $HOME/$1"
-    cp $HOME/dotfiles/$1 $HOME/$1
+    cp "$HOME/dotfiles/$1" "$HOME/$1"
   fi
 }
 
@@ -47,7 +47,7 @@ symlink() {
     echo "skip making link: $HOME/$1 exists"
   else
     echo "make link $HOME/dotfiles/$1 to $HOME/$1"
-    ln -s $HOME/dotfiles/$1 $HOME/$1
+    ln -s "$HOME/dotfiles/$1" "$HOME/$1"
   fi
 }
 
@@ -59,8 +59,8 @@ symlink .inputrc
 symlink .tmux.conf
 symlink .tigrc
 
-[[ ! -d $HOME/.vim/pack/minpac ]] && mkdir -p $HOME/.vim/pack/minpac/{start,opt}
-[[ ! -d $HOME/.vim/pack/minpac/opt/minpac ]] && git clone https://github.com/k-takata/minpac.git $HOME/.vim/pack/minpac/opt/minpac
+[[ ! -d $HOME/.vim/pack/minpac ]] && mkdir -p "$HOME/.vim/pack/minpac/"{start,opt}
+[[ ! -d $HOME/.vim/pack/minpac/opt/minpac ]] && git clone https://github.com/k-takata/minpac.git "$HOME/.vim/pack/minpac/opt/minpac"
 symlink .vim/ultisnips
 symlink .vim/after
 
