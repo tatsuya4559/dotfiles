adc9232cf9e91c766a72aa50cafebb0a1b6bd16a
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-09-10 13:07:03 +0900

    Add install script add completion

---
+++ plug/plug_completion.sh
@@ -0,0 +1,24 @@
+# bash completion for plug
+_plug() {
+  local cur prev words cword
+  _get_comp_words_by_ref -n : cur prev words cword
+
+  case "${prev}" in
+    plug)
+      local subcommands="install uninstall ls enable disable"
+      COMPREPLY=( $(compgen -W "${subcommands}" -- "${cur}") )
+      ;;
+    uninstall)
+      local plugins=$(cat <(\ls ~/.vim/pack/plug/start) <(\ls ~/.vim/pack/plug/disabled))
+      COMPREPLY=( $(compgen -W "${plugins}" -- "${cur}") )
+      ;;
+    enable)
+      local plugins=$(\ls $HOME/.vim/pack/plug/disabled)
+      COMPREPLY=( $(compgen -W "${plugins}" -- "${cur}") )
+      ;;
+    disable)
+      local plugins=$(\ls $HOME/.vim/pack/plug/start)
+      COMPREPLY=( $(compgen -W "${plugins}" -- "${cur}") )
+      ;;
+  esac
+} && complete -F _plug plug
