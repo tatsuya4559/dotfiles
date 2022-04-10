1d1a7489d1fb06ad47e5962efb01c3f35d7aa551
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-04-10 20:42:22 +0900

    manage git-prompt and git-completion by chezmoiexternal.yaml

--- chezmoi/run_once_020_install_git_utils.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'download .git-completion.bash'
-curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$HOME/.git-completion.bash"
-chmod a+x "$HOME/.git-completion.bash"
-
-echo 'download .git-prompt.sh'
-curl -fsSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$HOME/.git-prompt.sh"
-chmod a+x "$HOME/.git-prompt.sh"
