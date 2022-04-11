e817c5c8951fb3851cb52acb8a7295e1ae366b72
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-04-12 08:03:46 +0900

    add heroku and aws cli

---
+++ chezmoi/run_onchange_install_asdf_plugins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+asdf plugin add nodejs
+asdf install nodejs 16.14.2
+
+asdf plugin add python
+asdf install python 3.9.11
+
+asdf plugin add terraform
+asdf install terraform 1.1.5
+
+asdf plugin add golang
+asdf install golang 1.18
+
+asdf plugin add awscli
+asdf install awscli 2.5.4
+
+asdf plugin add heroku-cli
+asdf install heroku-cli 7.60.1
