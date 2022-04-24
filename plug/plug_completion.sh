f162f8c52e2e1fc845510518d3a28a9b9791bbd5
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-04-24 16:33:39 +0900

    shellcheck and shfmt

--- plug/plug_completion.sh
+++ plug/plug_completion.sh
@@ -1,29 +1,30 @@
+#!/bin/bash
 # bash completion for plug
 _plug() {
   local cur prev words cword
   _get_comp_words_by_ref -n : cur prev words cword
 
-  if [[ ${cword} > 2 ]]; then
+  if [[ ${cword} -gt 2 ]]; then
     # make completion based on subcommand
     prev=${words[1]}
   fi
 
   case "${prev}" in
-    plug)
-      local subcommands="install uninstall ls enable disable"
-      COMPREPLY=( $(compgen -W "${subcommands}" -- "${cur}") )
-      ;;
-    uninstall)
-      local plugins=$(cat <(\ls ~/.vim/pack/plug/start) <(\ls ~/.vim/pack/plug/disabled))
-      COMPREPLY=( $(compgen -W "${plugins}" -- "${cur}") )
-      ;;
-    enable)
-      local plugins=$(\ls $HOME/.vim/pack/plug/disabled)
-      COMPREPLY=( $(compgen -W "${plugins}" -- "${cur}") )
-      ;;
-    disable)
-      local plugins=$(\ls $HOME/.vim/pack/plug/start)
-      COMPREPLY=( $(compgen -W "${plugins}" -- "${cur}") )
-      ;;
+  plug)
+    local subcommands="install uninstall ls enable disable"
+    COMPREPLY=($(compgen -W "${subcommands}" -- "${cur}"))
+    ;;
+  uninstall)
+    local plugins=$(cat <(\ls ~/.vim/pack/plug/start) <(\ls ~/.vim/pack/plug/disabled))
+    COMPREPLY=($(compgen -W "${plugins}" -- "${cur}"))
+    ;;
+  enable)
+    local plugins=$(\ls $HOME/.vim/pack/plug/disabled)
+    COMPREPLY=($(compgen -W "${plugins}" -- "${cur}"))
+    ;;
+  disable)
+    local plugins=$(\ls $HOME/.vim/pack/plug/start)
+    COMPREPLY=($(compgen -W "${plugins}" -- "${cur}"))
+    ;;
   esac
 } && complete -F _plug plug
