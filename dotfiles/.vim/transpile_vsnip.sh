609009c150bd7aff73592c68e8acfe783ab2ba61
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-06-20 08:04:12 +0900

    fix: trim \n

--- dotfiles/.vim/transpile_vsnip.sh
+++ dotfiles/.vim/transpile_vsnip.sh
@@ -4,8 +4,7 @@ set -eu
 function transpile() {
   local -r filepath="$1"
   local -r outputpath="$2"
-  # FIXME: remove last line
-  cat "${filepath}" | yq -o json | jq 'map_values(.body |= split("\n"))' > "${outputpath}"
+  cat "${filepath}" | yq -o json | jq 'map_values(.body |= (rtrimstr("\n") | split("\n")))' > "${outputpath}"
 }
 
 for filepath in $(find ~/.vim/vsnip/src/ -type f); do
