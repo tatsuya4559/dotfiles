87bea5af315f89a2a03343fdb561a5b53c2258be
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-07-17 16:57:20 +0900

    export TERM for docker image building

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,12 +2,18 @@
 set -eu
 
 ok() {
+  if [[ -z $TERM ]]; then
+    export TERM=xterm
+  fi
   tput setaf 2
   echo "$@"
   tput sgr0
 }
 
 err() {
+  if [[ -z $TERM ]]; then
+    export TERM=xterm
+  fi
   tput setaf 1
   echo "$@"
   tput sgr0
